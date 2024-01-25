7070ba4ad19b38e2776a9328192197ae7d737db7
Garvit Arora <aroragarvit1805@gmail.com>
2024-01-25 10:29:05 +0530

    reply_emial

---
+++ script.js
@@ -0,0 +1,51 @@
+const chattingtext = document.querySelector(".chating_text");
+const chat = document.querySelector(".chat");
+const massage_hii = document.createElement("button");
+massage_hii.setAttribute("class", "massage_hii");
+massage_hii.innerHTML = "Hii!!👋🏻";
+chat.append(massage_hii);
+
+massage_hii.addEventListener("click", () => {
+    form();
+    massage_hii.style.display = "none";
+});
+
+function form() {
+    const form = document.createElement("form");
+    let nam = document.createElement("input");
+    nam.setAttribute("class", "nam");
+    let mail = document.createElement("input");
+    mail.setAttribute("class", "mail");
+    let msg = document.createElement("input");
+    msg.setAttribute("class", "msg");
+    let submitbtn = document.createElement("button");
+    submitbtn.setAttribute("class", "submitbtn");
+    chat.append(form);
+    form.append(nam);
+    form.append(mail);
+    form.append(msg);
+    form.append(submitbtn);
+    nam.type = "text";
+    nam.placeholder = "What's Your name?";
+    mail.type = "text";
+    mail.placeholder = "Type Your E-mail..";
+    msg.type = "text";
+    msg.placeholder = "Your Message to Me...?";
+    submitbtn.innerHTML = "Send Message😊";
+    submitbtn.type = "submit";
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        sendmail(nam.value, mail.value, msg.value);
+    });
+}
+
+function sendmail(name, email, massage) {
+    let takengiven = {
+        name: name,
+        email: email,
+        massage: massage,
+    }
+        emailjs.send("service_ppj72c7","template_jcvns8q",takengiven).then(alert("email send successfully!"));
+    }   
+
